7e8f64fbfbde4489ded193bf966c6a0e0fd58c9c
Ruben de Laat <ruben@logic-labs.nl>
2018-11-15 17:17:36 +0100

    Added timeouts

--- BcfAggregationService/src/org/opensourcebim/VoxelServerClient.java
+++ BcfAggregationService/src/org/opensourcebim/VoxelServerClient.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import org.apache.http.HttpEntity;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
@@ -13,22 +14,33 @@ import org.apache.http.entity.mime.HttpMultipartMode;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class VoxelServerClient implements AutoCloseable {
-	private CloseableHttpClient httpClient;
 	private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
-	private String baseAddress;
+	private static final Logger LOGGER = LoggerFactory.getLogger(VoxelServerClient.class);
+	private final CloseableHttpClient httpClient;
+	private final String baseAddress;
+	private final RequestConfig requestConfig;
 
 	public VoxelServerClient(String baseAddress) {
 		this.baseAddress = baseAddress;
 		httpClient = HttpClients.createDefault();
+		
+		RequestConfig.Builder requestConfigBuilder = RequestConfig.copy(RequestConfig.DEFAULT);
+		requestConfig = requestConfigBuilder.setConnectTimeout(2000)
+	        .setSocketTimeout(2000)
+	        .setConnectionRequestTimeout(2000)
+	        .build();
 	}
 	
 	public double getSurfaceArea(InputStream ifcInputStream) throws Exception {
 		HttpPost post = new HttpPost(baseAddress + "/surface_area");
+		post.setConfig(requestConfig);
 		
 		HttpEntity data = MultipartEntityBuilder.create()
             .setMode(HttpMultipartMode.BROWSER_COMPATIBLE)
@@ -56,7 +68,7 @@ public class VoxelServerClient implements AutoCloseable {
 		try {
 			httpClient.close();
 		} catch (IOException e) {
-			e.printStackTrace();
+			LOGGER.error("", e);
 		}
 	}
 }
